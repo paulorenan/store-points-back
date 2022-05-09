c46cd93b75b491963b06e977d1125bcf1395fc5c
Paulo Renan <able.paulorenan@gmail.com>
2022-05-09 01:55:02 -0300

    swagger doc

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const Routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
 
 const app = express();
 
@@ -9,9 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', Routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Hello, if you are looking for the API documentation, please visit /api-docs');
 });
 
 module.exports = app;
